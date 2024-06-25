63ab310f431c511bc6c6a4c8cffa18545a951392
alexisbl14 <ablydon@ucdavis.edu>
2024-06-25 12:02:58 -0700

    create basic navigation items

--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -1,7 +1,17 @@
+import logo from './logo.svg';
+
 function Nav() {
     return(
         <nav>
-
+            <img src={logo} alt="Logo"/>
+            <ul>
+                <li><a href="./Home">Home</a></li>
+                <li><a href="./About">About</a></li>
+                <li><a href="./Menu">Menu</a></li>
+                <li><a href="./Reservations">Reservations</a></li>
+                <li><a href="./Order">Order Online</a></li>
+                <li><a href="./Login">Login</a></li>
+            </ul>
         </nav>
     )
 }
