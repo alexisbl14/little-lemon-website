41f7fe4e8f230c231331b06e99a4e6c29f46dd19
alexisbl14 <ablydon@ucdavis.edu>
2024-06-25 12:07:42 -0700

    added necessary footer elements

--- little-lemon/src/Footer.js
+++ little-lemon/src/Footer.js
@@ -1,7 +1,18 @@
 function Footer() {
     return (
         <footer>
-
+            <span>
+                <h3>Contact</h3>
+                <p>Address</p>
+                <p>Phone Number</p>
+                <p>Email</p>
+            </span>
+            <span>
+                <h3>Social Media Links</h3>
+                <p>Twitter</p>
+                <p>Instagram</p>
+                <p>Facebook</p>
+            </span>
         </footer>
     )
 }
