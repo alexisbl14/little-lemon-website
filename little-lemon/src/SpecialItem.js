bccfa9f35c6557faa7ac228c34bce187a7de2cf8
alexisbl14 <ablydon@ucdavis.edu>
2024-06-26 14:38:10 -0700

    implement basic grid layout for specials

---
+++ little-lemon/src/SpecialItem.js
@@ -0,0 +1,10 @@
+function SpecialItem(props) {
+    return (
+        <div className={props.className}>
+            <h6>Greek Salad</h6>
+            <h5>Order a delivery</h5>
+        </div>
+    )
+}
+
+export default SpecialItem;
--- little-lemon/src/Specials.js
+++ little-lemon/src/Specials.js
@@ -1,7 +1,15 @@
+import SpecialItem from "./SpecialItem";
+
 function Specials() {
     return (
         <>
-            <p>About Page</p>
+            <div className="specials">
+                <h1 className="grid-item">This week's specials!</h1>
+                <button className="grid-item" type="button" onClick={() => console.log("button clicked")}><h4>Online Menu</h4></button>
+                <SpecialItem className="grid-item"/>
+                <SpecialItem className="grid-item"/>
+                <SpecialItem className="grid-item"/>
+            </div>
         </>
     )
 }
