e757e4dd7c958a4320f5b7bc0d57016371b6fe4d
alexisbl14 <ablydon@ucdavis.edu>
2024-06-27 13:07:35 -0700

    finished styling specials section

--- little-lemon/src/SpecialItem.js
+++ little-lemon/src/SpecialItem.js
@@ -1,8 +1,20 @@
+import deliveryIcon from "./delivery-icon.svg";
+
 function SpecialItem(props) {
     return (
-        <div className={props.className}>
-            <h6>Greek Salad</h6>
-            <h5>Order a delivery</h5>
+        <div className={`${props.className} special-item`}>
+            <img src={props.item.img} alt={props.item.name} className="special-img"/>
+            <div className="special-info">
+                <div>
+                    <h6 className="black-text">{props.item.name}</h6>
+                    <h5 className="red-text">${(props.item.price).toFixed(2)}</h5>
+                </div>
+                <p className="green-text">{props.item.description}</p>
+                <div>
+                    <h5 className="black-text">Order a delivery</h5>
+                    <img src={deliveryIcon} alt="Delivery"/>
+                </div>
+            </div>
         </div>
     )
 }
--- little-lemon/src/Specials.js
+++ little-lemon/src/Specials.js
@@ -1,14 +1,14 @@
 import SpecialItem from "./SpecialItem";
 
-function Specials() {
+function Specials(props) {
     return (
         <>
             <div className="specials">
                 <h1 className="grid-item black-text">This week's specials!</h1>
                 <button className="grid-item" type="button" onClick={() => console.log("button clicked")}><h4 className="black-text">Online Menu</h4></button>
-                <SpecialItem className="grid-item"/>
-                <SpecialItem className="grid-item"/>
-                <SpecialItem className="grid-item"/>
+                <SpecialItem item={props.specials[0]} className="grid-item"/>
+                <SpecialItem item={props.specials[1]} className="grid-item"/>
+                <SpecialItem item={props.specials[2]} className="grid-item"/>
             </div>
         </>
     )
