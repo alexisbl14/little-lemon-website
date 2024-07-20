abde9ab2566668ee4fd874b3f12d7281f81b9013
alexisbl14 <ablydon@ucdavis.edu>
2024-07-20 13:03:24 -0700

    finished implementing simple tests for update and initialize times

--- little-lemon/src/App.test.js
+++ little-lemon/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, act } from '@testing-library/react';
 import BookingForm from './BookingForm';
+import { initializeTimes, updateTimes } from './Nav';
 
 test('renders the BookingForm Heading', () => {
   const availableTimes = [];
@@ -8,3 +9,18 @@ test('renders the BookingForm Heading', () => {
   const headingElement = screen.getByText("Book a Table");
   expect(headingElement).toBeInTheDocument();
 });
+
+test('validate initializeTimes function', () => {
+  expect(initializeTimes()).toStrictEqual([])
+});
+
+describe('validate updateTimes function', () => {
+  test('returns times for date', () => {
+    expect(updateTimes({date: "2024-07-20"})).toStrictEqual(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"])
+  });
+
+  test('returns no times for no date', () => {
+    expect(updateTimes({date: ""})).toStrictEqual([])
+  });
+});
+
--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -9,19 +9,21 @@ import Booking from './Booking';
 import Order from './Order';
 import Login from './Login';
 
-const updateTimes = (state, action) => {
+export const updateTimes = (action) => {
     if(action.date === "") {return []};
     return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
 }
 
+export const initializeTimes = () => {
+    return [];
+}
+
+
 function Nav() {
-    const initializeTimes = () => {
-        return [];
-    }
+
     const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
 
     console.log(availableTimes);
-    
     return(
         <div>
             <nav>
