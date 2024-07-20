ef957db472b2592d58040df397004972b5a694ae
alexisbl14 <ablydon@ucdavis.edu>
2024-07-20 09:40:53 -0700

    added first unit test to check for rendered text

--- little-lemon/src/App.test.js
+++ little-lemon/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, act } from '@testing-library/react';
+import BookingForm from './BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the BookingForm Heading', () => {
+  const availableTimes = [];
+  const setAvailableTimes = jest.fn();
+  render(<BookingForm availableTimes={availableTimes} setAvailableTimes={setAvailableTimes}/>);
+  const headingElement = screen.getByText("Book a Table");
+  expect(headingElement).toBeInTheDocument();
 });
